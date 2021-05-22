5abd0f7efab103de63c34d315c9caf8342e8c852
TakumaInoue <inouetakuma19921228@gmail.com>
2021-05-22 13:28:46 +0900

    2021/05/22

--- sample10.php
+++ sample10.php
@@ -19,6 +19,32 @@
 <h2>Practice</h2>
 <pre>
 <!-- ここにプログラムを記述します -->
+<?php 
+  date_default_timezone_set('Asia/tokyo');
+  
+  print('現在は'. date('G'). '時です。'.  "\n");
+
+  if(date('G') < 11){
+    print('※現在の時間は営業時間外です。');
+  } else{
+    print('ようこそ。'. "\n");
+  }
+
+
+
+  $x = "takuma";
+  // if($x = true){
+  //   print ($x);
+  // }else{
+  //   print ("xには文字が入っていません。");
+  // }
+  if($x){
+    print ($x);
+  }else{
+    print ("xには文字が入っていません。");
+  }
+  // print ($x);
+?>
 </pre>
 </main>
 </body>    
