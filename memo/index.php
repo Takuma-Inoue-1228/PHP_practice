0ee272cc79ca7e73488c730cfa3868f43b485c1e
TakumaInoue <inouetakuma19921228@gmail.com>
2021-05-30 10:53:40 +0900

    2021/05/30 10:53:39

--- memo/index.php
+++ memo/index.php
@@ -25,12 +25,12 @@ try{
     echo 'DB接続エラー：' . $e->getMessage();
 }
 
-$count = $db->exec('INSERT INTO my_items SET maker_id=1,item_name="もも", item_name_kana="モモ", price=210, keyword="缶詰, ピンク, 甘い"');
-echo $count.'件のデータを挿入しました。';
-
+$records = $db->query('SELECT * FROM my_items');
+while ($record = $records->fetch()){
+  print($record['item_name'] . "\n");
+}
 
 ?>
-
 </pre>
 </main>
 </body>    
