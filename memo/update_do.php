95879d6cf897f9d98bb0465eadb528eaec21ff12
TakumaInoue <inouetakuma19921228@gmail.com>
2021-06-05 12:11:15 +0900

    2021/06/05 12:11:14

---
+++ memo/update_do.php
@@ -0,0 +1,33 @@
+<!doctype html>
+<html lang= 
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+<!-- Bootstrap CSS -->
+<link rel="stylesheet" href="../css/style.css">
+
+<title>PHP</title>
+</head>
+<body>
+<header>
+<h1 class="font-weight-normal">PHP</h1>    
+</header>
+
+<main>
+<h2>Practice</h2>
+<pre>
+<?php
+
+  require ('dbconect.php');
+
+  $statement = $db->prepare('UPDATE memos SET memo=? WHERE id=?');
+  $statement->execute(array($_POST['memo'], $_POST['id']));
+
+?>
+
+</pre>
+<p>メモの内容を変更しました</p>
+<a href="index.php">一覧へ</a>
+</main>
+</body>    
+</html>
