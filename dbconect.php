8b230ade19bdb80473112458df1083c76b125e9e
TakumaInoue <inouetakuma19921228@gmail.com>
2021-07-05 23:25:09 +0900

    connect_DB

--- dbconect.php
+++ dbconect.php
@@ -1,6 +1,10 @@
 <?php 
+  $dsn = 'mysql:host=mysql152.phy.lolipop.lan;dbname=LAA1239491-memodb;charset=utf8';
+  $user = 'LAA1239491';
+  $pass = '19921228';
+
   try{
-    $db = new PDO('mysql:dbname=mydb;host=localhost;port=8889;charset=utf8','root','root');
+    $db = new PDO($dsn,$user,$pass);
   } catch(PDOExceqtion $e) {
       echo 'DB接続エラー：' . $e->getMessage();
   }
