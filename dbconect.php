3848ba02f1faad9ebc115837c2689911b5da5dd9
Takuma-Inoue-1228 <61685148+Takuma-Inoue-1228@users.noreply.github.com>
2021-07-06 09:56:32 +0900

    Update dbconect.php

--- dbconect.php
+++ dbconect.php
@@ -1,7 +1,7 @@
 <?php 
-  $dsn = 'mysql:host=mysql152.phy.lolipop.lan;dbname=LAA1239491-memodb;charset=utf8';
-  $user = 'LAA1239491';
-  $pass = '19921228';
+  $dsn = 'mysql:host=***;dbname=***;charset=utf8';
+  $user = '***';
+  $pass = '***';
 
   try{
     $db = new PDO($dsn,$user,$pass);
